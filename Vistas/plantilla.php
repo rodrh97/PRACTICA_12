6c3abbd3c5a1340462c3abc2f277ba9350647539
Rodrigo <1530219@upv.edu.mx>
2018-10-26 21:11:14 -0500

    first commit

---
+++ Vistas/plantilla.php
@@ -0,0 +1,38 @@
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <link rel="icon" href="Diseños/assets/images/favicon.ico" type="image/x-icon">
+    <!-- Google font-->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">
+    <!-- Required Fremwork -->
+    <link rel="stylesheet" type="text/css" href="Diseños/components/bootstrap/css/bootstrap.min.css">
+    <!-- themify-icons line icon -->
+    <link rel="stylesheet" type="text/css" href="Diseños/assets/icon/themify-icons/themify-icons.css">
+    <!-- ico font -->
+    <link rel="stylesheet" type="text/css" href="Diseños/assets/icon/icofont/css/icofont.css">
+    <!-- Menu-Search css -->
+    <link rel="stylesheet" type="text/css" href="Diseños/assets/pages/menu-search/css/component.css">
+    <!-- Style.css -->
+    <link rel="stylesheet" type="text/css" href="Diseños/assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="Diseños/assets/css/jquery.mCustomScrollbar.css">
+    </head>
+    <body>
+    
+
+    <script type="text/javascript" src="Diseños/components/jquery/js/jquery.min.js"></script>
+    <script type="text/javascript" src="Diseños/components/jquery-ui/js/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="Diseños/components/popper.js/js/popper.min.js"></script>
+    <script type="text/javascript" src="Diseños/components/bootstrap/js/bootstrap.min.js"></script>
+    <!-- jquery slimscroll js -->
+    <script type="text/javascript" src="Diseños/components/jquery-slimscroll/js/jquery.slimscroll.js"></script>
+    <!-- modernizr js -->
+    <script type="text/javascript" src="Diseños/components/modernizr/js/modernizr.js"></script>
+    <script type="text/javascript" src="Diseños/components/modernizr/js/css-scrollbars.js"></script>
+    <!-- i18next.min.js -->
+    <script type="text/javascript" src="Diseños/components/i18next/js/i18next.min.js"></script>
+    <script type="text/javascript" src="Diseños/components/i18next-xhr-backend/js/i18nextXHRBackend.min.js"></script>
+    <script type="text/javascript" src="Diseños/components/i18next-browser-languagedetector/js/i18nextBrowserLanguageDetector.min.js"></script>
+    <script type="text/javascript" src="Diseños/components/jquery-i18next/js/jquery-i18next.min.js"></script>
+    <script type="text/javascript" src="Diseños/assets/js/common-pages.js"></script>
+    </body>
+    </html>
